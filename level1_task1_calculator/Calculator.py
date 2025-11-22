afc3abbded952f13f3c4b7facde2e7038166c39a
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-11-22 20:19:50 +0300

    prepared functions

---
+++ level1_task1_calculator/Calculator.py
@@ -0,0 +1,21 @@
+#simple calculator app
+
+#add fucntion
+def add(x, y):
+    return x + y
+
+#subtract function
+def subtract(x, y):
+    return x - y
+
+#multiply function
+def multiply(x, y):
+    return x * y
+
+#divide function
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+
+
